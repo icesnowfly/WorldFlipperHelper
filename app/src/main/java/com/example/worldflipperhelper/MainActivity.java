ddc1ae55453b837bb51f5f73c8e1faa21beac582
XINDONG <jiangsiren@xd.com>
2021-11-11 13:57:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/worldflipperhelper/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.worldflipperhelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
